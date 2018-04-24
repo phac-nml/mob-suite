803d2156a774247b1e7da8a1beb9eb207bc3e3b2
James Robertson <jrober84@gmail.com>
2018-04-24 14:03:23 -0400

    bug fix setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ Topic :: Scientific/Engineering
 Topic :: Scientific/Engineering :: Bio-Informatics
 Programming Language :: Python
 Programming Language :: Python :: 2.7
-Programming Language :: Python :: 3.4
-Programming Language :: Python :: 3.5
-Programming Language :: Python :: 3.6
 Programming Language :: Python :: Implementation :: CPython
 Operating System :: POSIX :: Linux
 """.strip().split('\n')
