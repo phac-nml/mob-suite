7a8ea7d75916fd5bc7f87a7f85de5064ae0868e6
Dillon Barker <dor.barker@gmail.com>
2019-03-08 12:26:04 -0700

    Remove dead code; tidy and document extract

--- mob_suite/mob_init.py
+++ mob_suite/mob_init.py
@@ -76,27 +76,33 @@ def download_to_file(url,file):
         c.perform()
         c.close()
 
-def extract(fname,outdir):
-    if (fname.endswith("tar.gz")):
-        tar = tarfile.open(fname, "r:gz")
-        tar.extractall()
-        tar.close()
-    elif (fname.endswith("tar")):
-        tar = tarfile.open(fname, "r:")
-        tar.extractall(outdir)
-        tar.close()
-    elif(fname.endswith("zip")):
-        zip_ref = zipfile.ZipFile(fname, 'r')
-        zip_ref.extractall(outdir)
-        zip_ref.close()
-    elif(fname.endswith("gz")):
-        outfile = os.path.join(outdir,fname.replace('.gz',''))
-        with gzip.open(fname, 'rb') as f_in:
-            with open(outfile, 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-            f_in.close()
-            f_out.close()
-            os.remove(fname)
+
+def extract(fname, outdir):
+    """
+    Decompress a zip or gzip archive. Decompression method is selected based
+    on file extension. Following extraction, the original archive is deleted.
+
+    :param fname:  Path to the archive to be extracted
+    :param outdir: Directory into which the results are placed
+    :return: None
+    """
+
+    logging.info(f'Decompressing {fname}')
+
+    if fname.endswith(".zip"):
+
+        with zipfile.ZipFile(fname, 'r') as zip_ref:
+            zip_ref.extractall(outdir)
+
+    elif fname.endswith(".gz"):
+
+        outfile = os.path.join(outdir, fname.replace('.gz',''))
+
+        with gzip.open(fname, 'rb') as f_in, open(outfile, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+    os.remove(fname)
+
 
 def main():
 
@@ -117,9 +123,8 @@ def main():
     plasmid_database_fasta_file = os.path.join(database_directory,'ncbi_plasmid_full_seqs.fas')
     repetitive_fasta_file = os.path.join(database_directory,'repetitive.dna.fas')
     mash_db_file =  os.path.join(database_directory,'ncbi_plasmid_full_seqs.fas.msh')
+
     logging.info('Downloading databases...this will take some time')
-
-
 
     for db_mirror in config['db_mirrors']:
 
@@ -129,16 +134,14 @@ def main():
         if check_hash(zip_file, config['db_hash']):
             break   #do not try other mirror
 
-    else:
+    else:  # no break
 
         logging.error('Downloading databases failed, please check your internet connection and retry')
         sys.exit(-1)
 
-
     logging.info('Downloading databases successful, now building databases')
 
     extract(zip_file, database_directory)
-    os.remove(zip_file)
 
     files = [os.path.join(database_directory, f)
              for f in os.listdir(database_directory)
