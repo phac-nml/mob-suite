b70d357b8e6c8ede1374dfbd3fe2408709316a72
jrober84 <james.robertson@phac-aspc.gc.ca>
2019-12-23 09:30:37 -0500

    Delete testing_circularization.py

--- mob_suite/testing_circularization.py
+++
@@ -1,9 +0,0 @@
-from mob_suite.wrappers import detectCircularity
-
-
-fasta_file = '/Users/jrobertson/Desktop/test.fasta'
-out_dir = '/Users/jrobertson/Desktop/test_circularize'
-
-
-c = detectCircularity()
-c.run(fasta_file,out_dir)
