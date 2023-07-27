b2e6b750b7d7163d6bee2e6ed7dfa666b9474499
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-07-27 16:52:22 -0500

    Add missing comma to entry_points

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'Sequenoscope=Sequenoscope:main'
+            'Sequenoscope=Sequenoscope:main',
             'mob_init=mob_suite.mob_init:main',
             'mob_recon=mob_suite.mob_recon:main',
             'mob_cluster=mob_suite.mob_cluster:main',
